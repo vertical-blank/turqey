89a814772af1b0a31f89933571129c0739143aa8
yohei.yamana <fixeight@gmail.com>
2015-11-21 22:26:01 +0900

    login wip

--- src/main/scala/turqey/controller/LoginController.scala
+++ src/main/scala/turqey/controller/LoginController.scala
@@ -1,7 +1,9 @@
 package turqey.controller
 
+import scalikejdbc._
 import org.scalatra._
 import turqey.html
+import turqey.entity._
 
 class LoginController extends ControllerBase  {
   override val path = "/login"
@@ -9,6 +11,16 @@ class LoginController extends ControllerBase  {
 
   val login = get("/") {
     html.login()
+  }
+
+  post("/") {
+    val id   = params.get("id")
+    val pass = params.get("pass")
+
+    val digestedPass = turqey.utils.Digest.get(pass.get)
+
+    val user = User.findBy(sqls.eq(User.u.email, id).and.eq(User.u.password, pass))
+
   }
 
 }
