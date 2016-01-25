739db80d5abf17751eb3ba7e8e48ef25a5dcc9df
youhei.yamana <fixeight@gmail.com>
2016-01-25 21:05:05 +0900

    added mime-type detection method.

--- src/main/scala/turqey/utils/FileUtil.scala
+++ src/main/scala/turqey/utils/FileUtil.scala
@@ -3,6 +3,9 @@ package turqey.utils
 import collection.JavaConversions._
 import eu.medsea.mimeutil._
 
+import java.io.File
+import java.io.InputStream
+
 object FileUtil {
 
   def getMimeType(file: java.io.File): String = {
@@ -15,6 +18,22 @@ object FileUtil {
       case _
         => "application/octet-stream"
     }
+  }
+  
+  def isImage(file: java.io.File): Boolean = {
+    MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
+    val mimeTypes = MimeUtil.getMimeTypes(file);
+    
+    mimeTypes.headOption match {
+      case Some(mimeType: MimeType)
+        => mimeType.getMediaType() == "image"
+      case _
+        => false
+    }
+  }
+  
+  def saveFileTo(file: File, path: String): Unit = {
+    
   }
 
 }
