261738548a3565b82935b005f45acd41c631ff8b
yohei224 <fixeight@gmail.com>
2016-03-08 20:57:34 +0900

    trim wip.

--- src/main/webapp/assets/js/turqey.js
+++ src/main/webapp/assets/js/turqey.js
@@ -1,17 +1,11 @@
 
-function loadFile(file) {
+function loadFile(file, onload) {
   if (!/^image\/(png|jpeg|gif)$/.test(file.type)) return;
 
   var fr = new FileReader();
   fr.onload = function(evt) {
       var imageObj = new Image();
-      imageObj.onload = function() {
-        var canvas = document.getElementById('photo');
-        var context = canvas.getContext('2d');
-        canvas.width = imageObj.width;
-        canvas.height = imageObj.height;
-        context.drawImage(imageObj, 0, 0);
-      }
+      imageObj.onload = onload;
       imageObj.src = fr.result;
     }
   fr.readAsDataURL(file);
