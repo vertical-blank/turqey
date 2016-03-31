5cdacfa12c1150e0af1acdf39e417d42d990c6a1
yohei.yamana <fixeight@gmail.com>
2016-03-31 23:35:46 +0900

    Add LockByVal object

---
+++ src/main/scala/turqey/utils/LockByVal.scala
@@ -0,0 +1,26 @@
+package turqey.utils
+
+import scala.collection._
+import scala.collection.convert.decorateAsScala._
+import java.util.concurrent.locks._
+
+object LockByVal {
+
+  val objs: concurrent.Map[Any, Lock] = new java.util.concurrent.ConcurrentHashMap().asScala
+
+  def lock(key: Any): Lock = {
+    objs.putIfAbsent(key, new ReentrantLock())
+    objs(key)
+  }
+
+  def withLock(key: Any)(f: => Unit) = {
+    try {
+      lock(key).lock
+      f
+    }
+    finally {
+      lock(key).unlock
+    }
+  }
+}
+
